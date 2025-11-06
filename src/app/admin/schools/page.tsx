835d6d11926083baceee980660e1d625323e35f1
Verzional <verzional777@gmail.com>
2025-11-06 12:33:05 +0700

    Removed Extra Pagination

--- src/app/admin/schools/page.tsx
+++ src/app/admin/schools/page.tsx
@@ -1,13 +1,12 @@
 import { getAllSchools } from "@/services/school";
 import { SchoolList } from "@/components/admin/schools/list";
-import { Pagination } from "@/components/layout/pagination";
 
 export default async function SchoolsPage() {
   const schools = await getAllSchools();
 
   return (
     <>
-      <SchoolList schools={schools} /> <Pagination />
+      <SchoolList schools={schools} /> 
     </>
   );
 }
