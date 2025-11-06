3a7b2c952cf5edeaa4e544b732eaf58548ab7dfe
Verzional <verzional777@gmail.com>
2025-11-06 12:42:04 +0700

    Migrate to Soketi

--- src/lib/pusher.ts
+++ src/lib/pusher.ts
@@ -1,35 +1,17 @@
 import PusherServer from "pusher";
 import PusherClient from "pusher-js";
 
-// export const pusherClient = new PusherClient(
-//   process.env.NEXT_PUBLIC_PUSHER_KEY!,
-//   {
-//     cluster: process.env.NEXT_PUBLIC_PUSHER_CLUSTER!,
-//     wsHost: process.env.NEXT_PUBLIC_SOKETI_HOST!,
-//     wsPort: parseInt(process.env.NEXT_PUBLIC_SOKETI_PORT!, 10),
-//     wssPort: parseInt(process.env.NEXT_PUBLIC_SOKETI_PORT!, 10),
-//     forceTLS: process.env.NEXT_PUBLIC_SOKETI_TLS === "true",
-//     wsPath: process.env.NEXT_PUBLIC_SOKETI_PATH,
-//     disableStats: true,
-//     enabledTransports: ["ws", "wss"],
-//   }
-// );
-
-// export const pusherServer = new PusherServer({
-//   appId: process.env.PUSHER_APP_ID!,
-//   key: process.env.PUSHER_KEY!,
-//   secret: process.env.PUSHER_SECRET!,
-//   cluster: process.env.PUSHER_CLUSTER!,
-//   host: process.env.SOKETI_HOST!,
-//   port: process.env.SOKETI_PORT!,
-//   useTLS: process.env.SOKETI_TLS === "true",
-// });
-
 export const pusherClient = new PusherClient(
   process.env.NEXT_PUBLIC_PUSHER_KEY!,
   {
     cluster: process.env.NEXT_PUBLIC_PUSHER_CLUSTER!,
-    forceTLS: true,
+    wsHost: process.env.NEXT_PUBLIC_SOKETI_HOST!,
+    wsPort: parseInt(process.env.NEXT_PUBLIC_SOKETI_PORT!, 10),
+    wssPort: parseInt(process.env.NEXT_PUBLIC_SOKETI_PORT!, 10),
+    forceTLS: process.env.NEXT_PUBLIC_SOKETI_TLS === "true",
+    wsPath: process.env.NEXT_PUBLIC_SOKETI_PATH,
+    disableStats: true,
+    enabledTransports: ["ws", "wss"],
   }
 );
 
@@ -38,5 +20,7 @@ export const pusherServer = new PusherServer({
   key: process.env.PUSHER_KEY!,
   secret: process.env.PUSHER_SECRET!,
   cluster: process.env.PUSHER_CLUSTER!,
-  useTLS: true,
+  host: process.env.SOKETI_HOST!,
+  port: process.env.SOKETI_PORT!,
+  useTLS: process.env.SOKETI_TLS === "true",
 });
